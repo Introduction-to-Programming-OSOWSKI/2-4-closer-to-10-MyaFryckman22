2053acceb117ce01a09e6c70b1b3859d886d94eb
MyaFryckman22 <817258@mystma.org>
2021-09-28 15:12:02 -0500

    2

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
 #define Function 
 def close10(x,y):
-    if abs(10-x)>= abs(10-y):
+    if abs(10-x)> abs(10-y):
         return y
 
-    elif abs(10-x)<= abs(10-y):
+    elif abs(10-x)< abs(10-y):
         return x
 
     else:
-        return 0
+        return  0
 
-print(close10(5, 12))  
+print(close10(5, 15))  
